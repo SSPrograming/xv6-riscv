3623c6997a403e579f8814525a112b53535e6911
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2021-10-25 09:56:57 +0800

    add B

--- user/init.c
+++ user/init.c
@@ -25,7 +25,7 @@ main(void)
 
   for(;;){
     printf("init: starting sh\n");
-    printf("available memory: %l\n", kspare());
+    printf("available memory: %lB\n", kspare());
     pid = fork();
     if(pid < 0){
       printf("init: fork failed\n");
