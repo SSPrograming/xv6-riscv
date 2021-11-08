55fe2adc2aba34530d3fcae7fc6a1111aceac785
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2021-11-08 15:42:22 +0800

    add print a page table

--- kernel/proc.c
+++ kernel/proc.c
@@ -207,7 +207,7 @@ proc_pagetable(struct proc *p)
     return 0;
   }
 
-  // map the usyscall just below TRAPFRAM
+  // map the usyscall just below TRAPFRAM.
   if(mappages(pagetable, USYSCALL, PGSIZE,
               (uint64)(p->usyscall), PTE_R | PTE_U) < 0) {
     uvmunmap(pagetable, TRAMPOLINE, 2, 0);
